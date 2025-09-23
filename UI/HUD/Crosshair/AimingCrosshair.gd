44ef13fab6d46ebcf3b5294aba3a12633ae4d40d
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2025-09-23 09:09:01 -0400

    Mobile Fix

--- UI/HUD/Crosshair/AimingCrosshair.gd
+++ UI/HUD/Crosshair/AimingCrosshair.gd
@@ -3,7 +3,8 @@ extends Node2D
 var pressed : bool = false
 
 func _process(_delta):
-	global_position = round(get_global_mouse_position())
+	if !DisplayServer.is_touchscreen_available() or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		global_position = round(get_global_mouse_position())
 
 func _on_sprite_2d_button_down() -> void:
 	pressed = true
